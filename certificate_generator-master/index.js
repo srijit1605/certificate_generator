077b2f97207383243a94bd43805ce4115b0a45ef
Srijit Ghosh <srijit16thmay@gmail.com>
2023-02-27 14:40:17 +0530

    fixes

--- certificate_generator-master/index.js
+++ certificate_generator-master/index.js
@@ -17,9 +17,9 @@ const newData = {"version":"0.6","reqId":"0","status":"ok","sig":"965851464","ta
 
 
 $(document).ready(() => {
-  $.get(url, (data) => {
-    participants = JSON.parse(data.substring(47).slice(0, -2));
-    // participants = newData;
+  // $.get(url, (data) => {
+    // participants = JSON.parse(data.substring(47).slice(0, -2));
+    participants = newData;
     console.log(participants.table.rows);
     for (i = 0; i < participants.table.rows.length; i++) {
       var participantName = participants.table.rows[i].c[2].v;
@@ -33,7 +33,7 @@ $(document).ready(() => {
     }
     $("#participantList").show();
     $("#loading").hide();
-  });
+  // });
 });
 
 $("#participantList").submit((e) => {
